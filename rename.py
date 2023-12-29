ef3dc5c946f041d0b8215f091059760a48d7912f
roundspring2003 <hwilly40159@gmail.com>
2023-12-29 11:40:19 +0800

    2023/12/29

---
+++ rename.py
@@ -0,0 +1,17 @@
+# encoding: Big5
+import os
+
+folder_path = 'C:/Users/User/Documents/¿ý­µ'
+
+file_list = os.listdir(folder_path)
+
+i = 49
+for old_name in file_list:
+
+    new_name = f"left{i}.wav"
+
+    old_path = os.path.join(folder_path, old_name)
+    new_path = os.path.join(folder_path, new_name)
+    
+    os.rename(old_path, new_path)
+    i += 1
