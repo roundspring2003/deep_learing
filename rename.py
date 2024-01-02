afd9a67940c569e87a58813edd3271105923f5a1
roundspring2003 <hwilly40159@gmail.com>
2024-01-02 14:37:32 +0800

    2023/12/29_up

--- rename.py
+++ rename.py
@@ -5,10 +5,10 @@ folder_path = 'C:/Users/User/Documents/¿ý­µ'
 
 file_list = os.listdir(folder_path)
 
-i = 100
+i = 199
 for old_name in file_list:
 
-    new_name = f"front{i}.wav"
+    new_name = f"up{i}.wav"
 
     old_path = os.path.join(folder_path, old_name)
     new_path = os.path.join(folder_path, new_name)
